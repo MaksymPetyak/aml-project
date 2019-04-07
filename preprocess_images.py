43406a33925ec9c6e4b94dd5371270fa3936bfc9
Magtheridon96 <calculabaydoun@hotmail.com>
2019-04-07 13:54:52 +0300

    completed and refactored file that preprocesses images

--- preprocess_images.py
+++ preprocess_images.py
@@ -1,17 +1,25 @@
 import os
-from multiprocessing.pool import Pool
+import sys
+import multiprocessing.pool
 
-import click
+from PIL import Image
 import numpy as np
-import PIL
 import cv2
 
-white_list_extensions = ['jpg', 'jpeg', 'JPEG', 'tif']
+# The amount of concurrency available. This should not be set
+# higher than the number of hardware threads available on the
+# machine, minus 1.
+THREAD_COUNT = 3
+# The amount of images in a single batch. We divide the images
+# into batches and assign those batches to different threads.
+BATCH_SIZE = 500
+# The expected size of the processed images at the end.
+CROP_SIZE = 512
 
-def convert(file_name, crop_size):
-    image = PIL.Image.open(file_name, mode='r')
-    assert len(np.shape(image)) == 3, 'Shape of image {} unexpected'.format(file_name)
-    width, height = im.size
+def convert(filename, crop_size):
+    image = Image.open(filename, mode='r')
+    assert len(np.shape(image)) == 3, 'Shape of image {} unexpected'.format(filename)
+    width, height = image.size
     converted = None
     if width / float(height) >= 1.3:
         cols_thres = np.where(np.max(np.max(np.asarray(image), axis=2), axis=0) > 35)[0]
@@ -19,98 +27,93 @@ def convert(file_name, crop_size):
             min_x, max_x = cols_thres[0], cols_thres[-1]
         else:
             min_x, max_x = 0, -1
-        converted = image.crop((min_x, 0, max_x, h))
+        converted = image.crop((min_x, 0, max_x, height))
     else:
         converted = image
-    # TODO: Perhaps switch to Bibuc or LANCZOS?
     converted = converted.resize((crop_size, crop_size), resample=Image.BILINEAR)
-    enhanced_image = contrast_enhance(np.asarray(converted), radius=crop_size//2)
+    enhanced_image = enhance_contrast(np.asarray(converted), radius=crop_size//2)
     return Image.fromarray(enhanced_image.astype(np.uint8))
 
-def contrast_enhance(image, radius):
-    '''Subtract local average color and map local average to 50% gray
-    Parameters
-    ==========
-    image: array of shape (height, width, 3)
-    radius: int
-        for square images a good choice is size/2
-    Returns
-    =======
-    contrast enhanced image as array of shape (height, width, 3)
-    Reference
-    =========
-    B. Graham, "Kaggle diabetic retinopathy detection competition report",
-        University of Warwick, Tech. Rep., 2015
-    https://github.com/btgraham/SparseConvNet/blob/kaggle_Diabetic_Retinopathy_competition/Data/kaggleDiabeticRetinopathy/preprocessImages.py
-    '''
+def enhance_contrast(image, radius):
     radius = int(radius)
     b = np.zeros(image.shape)
     cv2.circle(b, (radius, radius), int(radius * 0.9), (1, 1, 1), -1, 8, 0)
     blurred = cv2.GaussianBlur(image, (0, 0), radius / 30)
     return cv2.addWeighted(image, 4, blurred, -4, 128)*b + 128*(1 - b)
 
-def get_convert_fname(file_name, extension, directory, convert_directory):
-    source_extension = file_name.split('.')[-1]
-    return file_name.replace(source_extension, extension).replace(directory, convert_directory)
+readonly_source_directory = None
+readonly_target_directory = None
+def set_readonly_directories(source, target):
+    global readonly_source_directory
+    global readonly_target_directory
+    readonly_source_directory = source
+    readonly_target_directory = target
 
-def create_dirs(paths):
-    for p in paths:
-        try:
-            os.makedirs(p)
-        except OSError:
-            pass
+def is_image_filename(filename):
+    return filename.lower().split('.')[-1] in ['jpg', 'jpeg', 'tif', 'png']
 
-def process(args):
-    fun, arg = args
-    directory, convert_directory, fname, crop_size, \
-        extension, enhance_contrast, ignore_grayscale = arg
-    convert_fname = get_convert_fname(fname, extension, directory,
-                                      convert_directory)
-    if not os.path.exists(convert_fname):
-        img = fun(fname, crop_size, enhance_contrast, ignore_grayscale)
+# Given a filename, this will build a path for the file in our
+# target directory and assign the correct extension.
+def build_image_target_path(filename):
+    target_filename = ''.join(filename.split('.')[:-1] + ['.jpeg'])
+    return os.path.join(readonly_target_directory, filename)
+
+# This will take a path-filename pair, and write
+# a processed image to our target directory.
+def process_image(source_image):
+    source_path, source_filename = source_image[0], source_image[1]
+    print('process_image:')
+    print('   source_path = {}'.format(source_path))
+    print('   source_filename = {}'.format(source_filename))
+    print('   readonly_source_directory: ' + readonly_source_directory)
+    print('   readonly_target_directory: ' + readonly_target_directory)
+    target_path = build_image_target_path(source_filename)
+    if not os.path.exists(target_path):
+        img = convert(source_path, CROP_SIZE)
         if img is not None:
-            save(img, convert_fname)
+            img.save(target_path, quality=97)
 
-def save(img, fname):
-    img.save(fname, quality=97)
-
-@click.command()
-@click.option('--source_dir', default='data/train', show_default=True,
-              help="Directory with original images.")
-@click.option('--target_dir', default='data/train_res',
-              show_default=True,
-              help="Where to save converted images.")
-@click.option('--crop_size', default=512, show_default=True,
-              help="Size of converted images.")
-@click.option('--extension', default='jpeg', show_default=True,
-              help="Filetype of converted images.")
-@click.option('--n_proc', default=1, show_default=True,
-              help="Number of processes for parallelization.")
-def main(source_dir, target_dir, crop_size, extension, n_proc):
-    '''Image conversion: crop, resize, (enhance colour contrast) and save.'''
+def ensure_directory_exists(directory):
     try:
-        os.mkdir(target_dir)
+        os.mkdir(directory)
     except OSError:
         pass
-    filenames = [os.path.join(dp, f) for dp, dn, fn in os.walk(source_dir)
-                 for f in fn if f.split('.')[-1] in white_list_extensions]
 
-    assert filenames, 'No valid filenames.'
-    print('Resizing images in {} to {}, this takes a while.'.format(source_dir, target_dir))
+# This function will construct a list of image
+# (filepath, filename) pairs from a given directory.
+def collect_source_images(directory):
+    source_images = []
+    for directory_prefix, _, filenames in os.walk(directory):
+        image_filenames = [filename for filename in filenames if is_image_filename(filename)]
+        source_images.extend([(os.path.join(directory_prefix, f), f) for f in image_filenames])
+    return source_images
 
-    n = len(filenames)
-    batchsize = 500
-    batches = n // batchsize + 1
-    pool = Pool(n_proc)
-    args = []
-    for f in filenames:
-        args.append((convert, (source_dir, target_dir, f, crop_size,
-                               extension, enhance_contrast, ignore_grayscale)))
-    for i in range(batches):
-        print("batch {:>2} / {}".format(i + 1, batches))
-        pool.map(process, args[i * batchsize: (i + 1) * batchsize])
-    pool.close()
-    print('done')
+# Entry point.
+def main():
+    ensure_directory_exists(readonly_target_directory)
+    source_images = collect_source_images(readonly_source_directory)
+    if not source_images:
+        print('Nothing to process.')
+        exit()
+
+    print('Preprocessing initiated.')
+    print('  Source: {}'.format(readonly_source_directory))
+    print('  Target: {}'.format(readonly_target_directory))
+
+    # This is actually ceil(len(filenames) / BATCH_SIZE)
+    batch_count = (len(source_images) + BATCH_SIZE - 1) // BATCH_SIZE
+    thread_pool = multiprocessing.pool.Pool(THREAD_COUNT, set_readonly_directories,
+                                            (readonly_source_directory, readonly_target_directory))
+    for i in range(batch_count):
+        print('Assigning batch {}/{}'.format(i + 1, batch_count))
+        thread_pool.map(process_image, source_images[i*BATCH_SIZE : (i+1)*BATCH_SIZE])
+    thread_pool.close()
+    print('Preprocessing complete.')
 
 if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print('Usage:')
+        print('  python preprocess_images.py <source_dir> <target_dir>')
+        exit()
+    set_readonly_directories(sys.argv[1], sys.argv[2])
     main()
